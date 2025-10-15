4e5a178a6a1c6431d5b4afd12a56884641370259
psalsa30 <pauloualopsalvador@gmail.com>
2025-10-15 14:04:57 +0800

    added files

---
+++ main.js
@@ -0,0 +1,39 @@
+/*fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => {
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource")
+        }
+        return response.json();
+    })
+
+
+    .then(data => console.log(data.name))
+    .catch(error => console.error(error));*/
+
+fetchData();
+
+    async function fetchData(){
+
+        try{
+
+            const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+            if(!response.ok){
+                throw new Error("Could not fetch resource")
+            }
+
+            const data = await response.json();
+            const pokemonSprite = data.sprites.front_default;
+            const imgElement = document.getElementById("pokemonSprite");
+            const pokemonWeight = document.getElementById("pokemonWeight")
+
+            imgElement.src = pokemonSprite;
+            imgElement.style.display = "block";
+            pokemonWeight.display = data.weight
+        }
+        catch(error){
+            console.error(error);
+        }
+    }
